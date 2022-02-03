10430fd2706a84c7daf3f159f92915243baec2e0
emna <boumaizaemna@gmail.com>
2022-02-03 15:52:19 +0100

    add

--- git_example/js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('DOMContentLoaded');
-});
-
-alert('Hello World!');
-setTimeout(function() {
-    alert('Hello Git!');
-}, 5000);
