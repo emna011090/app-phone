66393346d2fb2e2f808258deec2c948b8d2d3909
emna <boumaizaemna@gmail.com>
2022-02-10 16:52:12 +0100

    vue contact

---
+++ vue-contact-manager/src/components/ContactList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Contact </h5>
+    <p class="card-text" v-for="contact in list" v-bind:key="contact.id">{{ contact.name }}</p>
+      <p class="card-text"></p>
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'ContactList',
+    props: {
+        list: Array
+        },
+
+
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-contact-manager/src/views/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  
+<ContactList :list="contactList"/>
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import  ContactList from '@/components/ContactList.vue'
+
+export default {
+  name: 'Home',
+
+  components:{
+    ContactList,
+
+  },
+  data(){
+    return{
+      contactList:[
+        {
+          id:1,
+          name:"emna",
+          emai:"emna@gmail.com",
+          phone:"+216 55 478 962",
+          adresse:{
+            street:"ibn khaloun",
+            city:"ariana",
+          },
+
+        },
+        {
+           id:2,
+          name:"mehdi",
+          emai:"mehdi@gmail.com",
+          phone:"+216 22 478 962",
+          adresse:{
+            street:"saada",
+            city:"manouba",
+          },
+        },
+        { id:3,
+          name:"olfa",
+          emai:"olfa@gmail.com",
+          phone:"+216 55 458 962",
+          adresse:{
+            street:"rafeha",
+            city:"tunis",
+          },}
+      ]
+    }
+  }
+}
+</script>
